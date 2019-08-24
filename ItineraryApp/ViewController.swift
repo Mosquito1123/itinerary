7a14b1430ca58fa0504d53fef3adf676bfb8fd6b
Cosmin Iulian <cosmin.iulian03@gmail.com>
2019-08-24 21:21:14 +0300

    first commit

---
+++ ItineraryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ItineraryApp
+//
+//  Created by Cosmin Iulian on 24/08/2019.
+//  Copyright © 2019 Cosmin Iulian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
